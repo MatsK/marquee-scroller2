ddb5b7897576584d0b6108f3b109fa965a36debb
Ján Jurdik <64465772+Hedgie1@users.noreply.github.com>
2024-03-31 11:17:38 +0200

    Update Settings.h

--- marquee/Settings.h
+++ marquee/Settings.h
@@ -49,8 +49,6 @@ SOFTWARE.
 #include "OpenWeatherMapClient.h"
 #include "TimeDB.h"
 #include "NewsApiClient.h" 
-#include "OctoPrintClient.h"
-#include "PiHoleClient.h"
 
 //******************************
 // Start Settings
@@ -97,20 +95,6 @@ int ledRotation = 3;
 String timeDisplayTurnsOn = "06:30";  // 24 Hour Format HH:MM -- Leave blank for always on. (ie 05:30)
 String timeDisplayTurnsOff = "23:00"; // 24 Hour Format HH:MM -- Leave blank for always on. Both must be set to work.
 
-// OctoPrint Monitoring -- Monitor your 3D printer OctoPrint Server
-boolean OCTOPRINT_ENABLED = false;
-boolean OCTOPRINT_PROGRESS = true;
-String OctoPrintApiKey = "";  // ApiKey from your User Account on OctoPrint
-String OctoPrintServer = "";  // IP or Address of your OctoPrint Server (DO NOT include http://)
-int OctoPrintPort = 80;       // the port you are running your OctoPrint server on (usually 80);
-String OctoAuthUser = "";     // only used if you have haproxy or basic athentintication turned on (not default)
-String OctoAuthPass = "";     // only used with haproxy or basic auth (only needed if you must authenticate)
-
-// Pi-hole Client -- monitor basic stats from your Pi-hole server (see http://pi-hole.net)
-boolean USE_PIHOLE = false;   // Set true to display your Pi-hole details
-String PiHoleServer = "";     // IP or Address only (DO NOT include http://)
-int PiHolePort = 80;          // Port of your Pi-hole address (default 80)
-String PiHoleApiKey = "";   // Optional -- only needed to see top blocked clients
 
 boolean ENABLE_OTA = true;    // this will allow you to load firmware to the device over WiFi (see OTA for ESP8266)
 String OTA_Password = "";     // Set an OTA password here -- leave blank if you don't want to be prompted for password
